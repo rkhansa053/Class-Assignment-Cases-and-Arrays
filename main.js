5d5e3b8bbd2590231b2024f998ddd2589b2cb132
rkhansa053 <160234986+rkhansa053@users.noreply.github.com>
2024-03-07 20:08:15 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+//  CASES:
+//camelCase.
+var fruitName = 'Apple';
+console.log(fruitName);
+//snake_case.
+var fruit_name = 'Pine Apple';
+console.log(fruit_name);
+//PascalCase.
+var FruitName = 'Custard Apple';
+console.log(FruitName);
+// Cases in type string.
+var daysName = "Monday";
+var days_name = "Tuesday";
+var DaysName = "Wednesday";
+console.log(daysName, days_name, DaysName);
+// Cases in type number.
+var personAge = 15;
+var person_age = 20;
+var PersonAge = 25;
+console.log(personAge, person_age, PersonAge);
+// Cases in type boolean.
+var isStudent = true;
+var is_student = false;
+var IsStudent = true;
+console.log(isStudent, is_student, IsStudent);
+//  ARRAYS:
+var monthsName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+console.log(monthsName[5], monthsName[10]);
+var studentsName = ["Ali", "Zoya", "Khansa", "Sameer", "Alyana", "Ayesha", "Abdul Rahman", "Talha", "Safia", "Alishba"];
+studentsName[7] = "Muhammad Rohaan";
+console.log(studentsName);
---
+++ main.ts
@@ -0,0 +1,42 @@
+//  CASES:
+
+//camelCase.
+let fruitName = 'Apple';
+console.log(fruitName);
+
+//snake_case.
+let fruit_name = 'Pine Apple';
+console.log(fruit_name);
+
+//PascalCase.
+let FruitName = 'Custard Apple';
+console.log(FruitName);
+
+// Cases in type string.
+let daysName = "Monday";
+let days_name = "Tuesday";
+let DaysName = "Wednesday";
+console.log(daysName, days_name, DaysName);
+
+// Cases in type number.
+let personAge = 15;
+let person_age = 20;
+let PersonAge = 25;
+console.log(personAge, person_age, PersonAge);
+
+// Cases in type boolean.
+let isStudent = true;
+let is_student = false;
+let IsStudent = true;
+console.log(isStudent, is_student, IsStudent);
+
+
+
+//  ARRAYS:
+
+let monthsName : string[] = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+console.log(monthsName[5], monthsName[10]);
+
+let studentsName : string[] = ["Ali", "Zoya", "Khansa", "Sameer", "Alyana", "Ayesha", "Abdul Rahman", "Talha", "Safia", "Alishba"];
+studentsName[7] = "Muhammad Rohaan";
+console.log(studentsName);
